f4aa6d639a2a64a7ca47b4edac1dc8fcfa95fbd9
KhomZ <54626716+KhomZ@users.noreply.github.com>
2020-03-18 20:45:28 +0545

    Assets

---
+++ Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float increment;
+    private Vector2 targetPos;
+    public float maxY;
+    public float minY;
+    public int health = 3;
+    public GameObject moveEffect;
+    public Text healthDisplay;
+    public GameObject gameOver;
+    public GameObject dashSound;
+    
+    void Update()
+    {
+        healthDisplay.text = health.ToString();
+        if (health <= 0)
+        {
+           
+          
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxY)
+        {
+            Instantiate(dashSound, transform.position, Quaternion.identity);
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y + increment);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minY)
+        {
+            Instantiate(dashSound, transform.position, Quaternion.identity);
+            Instantiate(moveEffect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - increment);
+            transform.position = targetPos;
+        }
+    }
+}
---
+++ Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] obstaclePatterns;
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+    public float timeDecrease;
+    public float minTime = 0.65f;
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+            if (startTimeBtwSpawns > minTime)
+            {
+                startTimeBtwSpawns -= timeDecrease;
+            }
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
